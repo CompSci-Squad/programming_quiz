7ffb2159b38f3ca265d7c2273124654002057c3c
do-ald533 <douglas.aldred533@gmail.com>
2023-05-03 08:58:41 -0300

    feat: creating default repository

--- src/shared/repositories/default_repository.py
+++ src/shared/repositories/default_repository.py
@@ -1,12 +1,25 @@
 from typing import Generic, TypeVar
-
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-T = TypeVar('T')
+from src.shared.logger.logger import LOGGER
+
+T = TypeVar("T")
+
 
 class DefaultRepository(Generic[T]):
     __session: Session
-    def create(self, entity: T):
+
+    def __init__(self, session: Session):
+        self.__session = session
+
+    def create(self, entity: T) -> None:
         try:
             self.__session.add(entity)
             self.__session.commit()
+        except IntegrityError as e:
+            LOGGER.error(f"unexpected error when commiting entity {entity}, error: {e}")
+
+    def find_and_delete(self, id: str) -> None:
+        try:
+            self.__session.expire
