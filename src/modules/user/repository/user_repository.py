d903f0f5026ded79bbdd61ebc8c529d18836e53d
do-ald533 <douglas.aldred533@gmail.com>
2023-05-03 10:55:41 -0300

    feat: integrating userEntity

--- src/modules/user/repository/user_repository.py
+++ src/modules/user/repository/user_repository.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm.session import Session
 from typing import Type
 from src.shared.repositories.default_repository import DefaultRepository
-from src.modules.user.entities import 
+from src.modules.user.entities.user_entity import UserEntity
 
-class UserRepository(DefaultRepository):
+class UserRepository(DefaultRepository[UserEntity]):
     def __init__(self, session: Session, user: Type[UserEntity]):
         super().__init__(session, user)
