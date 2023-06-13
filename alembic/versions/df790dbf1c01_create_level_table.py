54d2ad9beedef0d7b523f01dbcd375cf78780b1f
do-ald533 <douglas.aldred533@gmail.com>
2023-06-13 00:45:29 -0300

    init migration

---
+++ alembic/versions/df790dbf1c01_create_level_table.py
@@ -0,0 +1,33 @@
+"""create level table
+
+Revision ID: df790dbf1c01
+Revises: 
+Create Date: 2023-06-12 19:46:12.447938
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'df790dbf1c01'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'level',
+        sa.Column('id', sa.Integer, autoincrement=True, primary_key=True),
+        sa.Column('question', sa.String(80), nullable=False),
+        sa.Column('right_answer', sa.String(80), nullable=False),
+        sa.Column('reward', sa.Integer, nullable=False),
+        sa.Column('wrong_answer_1', sa.String(80), nullable=False),
+        sa.Column('wrong_answer_2', sa.String(80), nullable=False),
+        sa.Column('wrong_answer_3', sa.String(80), nullable=False)
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('level')
